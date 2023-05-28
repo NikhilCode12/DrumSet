03230b27a58c804aed18e3cfe0fd43d0e6207d92
Nikhil Sharma <promaxchessace@gmail.com>
2023-05-28 12:33:09 +0530

    renamed files to add github deployment

--- script.js
+++ script.js
@@ -13,11 +13,13 @@ for(var i=0;i<7;i++){
     var buttonPress = document.querySelectorAll('.drum')[i].addEventListener("click",function(){    
         var btnText = this.textContent;
         makeSound(btnText);
+        buttonAnimation(btnText);
     });
 }
 
 document.addEventListener('keydown',function(event){
     makeSound(event.key);
+    buttonAnimation(event.key);
 });
 
 function makeSound(key){
@@ -51,4 +53,10 @@ function makeSound(key){
             audio.play();
         break;
     }
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){ activeButton.classList.remove("pressed") },0.5);
 }
