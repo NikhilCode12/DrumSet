c53461bf207ff6f616c4e7f75786fb1c4d9d759c
Nikhil Sharma <promaxchessace@gmail.com>
2023-05-28 12:01:57 +0530

    added keyboard key listeners

--- index.js
+++ index.js
@@ -9,41 +9,46 @@ var sounds = [
     'sounds/kick-bass.mp3'
 ]
 
-
 for(var i=0;i<7;i++){
-    var wBtn = document.querySelectorAll('.drum')[i].addEventListener("click",function(){
-        var btnHtml = this.textContent;
-
-        switch(btnHtml){
-            case "w":
-                var audio = new Audio(sounds[0]);
-                audio.play();
-            break;
-            case "a":
-                var audio = new Audio(sounds[1]);
-                audio.play();
-            break;
-            case "s":
-                var audio = new Audio(sounds[2]);
-                audio.play();
-            break;
-            case "d":
-                var audio = new Audio(sounds[3]);
-                audio.play();
-            break;
-            case "j":
-                var audio = new Audio(sounds[4]);
-                audio.play();
-            break;
-            case "k":
-                var audio = new Audio(sounds[5]);
-                audio.play();
-            break;
-            case "l":
-                var audio = new Audio(sounds[6]);
-                audio.play();
-            break;
-        }
+    var buttonPress = document.querySelectorAll('.drum')[i].addEventListener("click",function(){    
+        var btnText = this.textContent;
+        makeSound(btnText);
     });
 }
 
+document.addEventListener('keydown',function(event){
+    makeSound(event.key);
+});
+
+function makeSound(key){
+    switch(key){
+        case "w":
+            var audio = new Audio(sounds[0]);
+            audio.play();
+        break;
+        case "a":
+            var audio = new Audio(sounds[1]);
+            audio.play();
+        break;
+        case "s":
+            var audio = new Audio(sounds[2]);
+            audio.play();
+        break;
+        case "d":
+            var audio = new Audio(sounds[3]);
+            audio.play();
+        break;
+        case "j":
+            var audio = new Audio(sounds[4]);
+            audio.play();
+        break;
+        case "k":
+            var audio = new Audio(sounds[5]);
+            audio.play();
+        break;
+        case "l":
+            var audio = new Audio(sounds[6]);
+            audio.play();
+        break;
+    }
+}
